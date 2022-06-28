2bee3b834f9093408e61d57d0ff4f604c829317b
christo <christoslimit@gmail.com>
2022-06-28 15:46:16 +0530

    Transitions updated

---
+++ HelloSwiftUI/HelloSwiftUI/TransitionBootCamp.swift
@@ -0,0 +1,20 @@
+//
+//  TransitionBootCamp.swift
+//  HelloSwiftUI
+//
+//  Created by Christo Kumar on 28/06/22.
+//
+
+import SwiftUI
+
+struct TransitionBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransitionBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TransitionBootCamp()
+    }
+}
