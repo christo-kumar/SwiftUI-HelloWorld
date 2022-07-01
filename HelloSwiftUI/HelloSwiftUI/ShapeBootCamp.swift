d1c825825155172ecc374cd633282b72181c9f2b
christo <christoslimit@gmail.com>
2022-07-01 10:01:43 +0530

    Shapes Practice, fill, trim, stroke

--- HelloSwiftUI/HelloSwiftUI/ShapeBootCamp.swift
+++ HelloSwiftUI/HelloSwiftUI/ShapeBootCamp.swift
@@ -9,16 +9,38 @@ import SwiftUI
 
 struct ShapeBootCamp: View {
     var body: some View {
-        Circle()
-            //.fill(.red)
-            .trim(from: 0.1, to: 1.0)
-            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .bevel, miterLimit: 1.0, dash: [40], dashPhase: 1.0))
-            
+        VStack {
+            Circle()
+            //.fill(.blue)
+                .trim(from: 0.1, to: 1.0)
+                .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .bevel, miterLimit: 1.0, dash: [40], dashPhase: 1.0))
+            PracticeShape()
+            Spacer()
+        }
     }
 }
 
 struct ShapeBootCamp_Previews: PreviewProvider {
     static var previews: some View {
         ShapeBootCamp()
+    }
+}
+
+struct PracticeShape: View {
+    var body : some View {
+        ZStack {
+            Rectangle()
+                .stroke(.blue, style: StrokeStyle(
+                    lineWidth: 10,
+                    lineCap: .butt,
+                    lineJoin: .bevel,
+                    miterLimit: 0,
+                    dash: [10],
+                    dashPhase: 1.0))
+            Rectangle()
+                .trim(from: 0.5, to: 1.0)
+                .fill(.red)
+                
+        }
     }
 }
