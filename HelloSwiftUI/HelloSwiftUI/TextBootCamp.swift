15103a04e96bd22aa0e7d0b2155f3cf85aef8773
christo <christoslimit@gmail.com>
2022-07-01 09:36:17 +0530

    Text Practice - Font, Underline, BaselineOffset, foregroundColor, frame,

--- HelloSwiftUI/HelloSwiftUI/TextBootCamp.swift
+++ HelloSwiftUI/HelloSwiftUI/TextBootCamp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TextBootCamp: View {
     let someStr = "Here I am this is me there no where on this earth I rather be."
     var body: some View {
+        VStack {
         Text(someStr)
 //            .font(.title)
 //            .bold()
@@ -23,11 +24,27 @@ struct TextBootCamp: View {
             .foregroundColor(.red)
             .frame(width: 200, height: 100, alignment: .center)
             .minimumScaleFactor(0.1)
+            TextPractice()
+    }
+        
     }
 }
 
 struct TextBootCamp_Previews: PreviewProvider {
     static var previews: some View {
         TextBootCamp()
+            
+    }
+}
+//font, underline offset foreground-color frame background-color
+struct TextPractice: View {
+    var body: some View {
+        Text("Practice")
+            .font(.system(size: 24, weight: .semibold, design: .serif))
+            .underline(true, color: .red)
+            .baselineOffset(10)
+            .foregroundColor(.green)
+            .frame(width: 100, height: 100, alignment: .center)
+            .background(Color.brown)
     }
 }
