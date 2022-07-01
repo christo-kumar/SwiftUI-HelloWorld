29499564b4d5e4f1a67c62e3f6bb7a5cd59c0cc3
christo <christoslimit@gmail.com>
2022-07-01 11:22:02 +0530

    Image - Resizeable, contentMode, Frame, ClipShape

--- HelloSwiftUI/HelloSwiftUI/ImageBootCamp.swift
+++ HelloSwiftUI/HelloSwiftUI/ImageBootCamp.swift
@@ -9,22 +9,35 @@ import SwiftUI
 
 struct ImageBootCamp: View {
     var body: some View {
-        Image("Prof_Pic2")
+        VStack {
+            Image("Prof_Pic2")
             //.renderingMode(.template)
             //.foregroundColor(.red)
-            .resizable()
-            .aspectRatio(contentMode: .fill)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
             //.scaledToFit()
             //.scaledToFill()
-            .frame(width: 300, height: 300, alignment: .center)
+                .frame(width: 300, height: 300, alignment: .center)
             //.clipped()
             //.cornerRadius(15.0)
-            .clipShape(Circle())
+                .clipShape(Circle())
+            ImagePractice()
+        }
     }
 }
 
 struct ImageBootCamp_Previews: PreviewProvider {
     static var previews: some View {
         ImageBootCamp()
+    }
+}
+
+struct ImagePractice: View {
+    var body : some View {
+        Image("Prof_Pic2")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 300, height: 300, alignment: .center)
+            .clipShape(Circle())
     }
 }
