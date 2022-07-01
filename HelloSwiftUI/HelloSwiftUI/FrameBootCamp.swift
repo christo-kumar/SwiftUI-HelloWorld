3b1eb240fd2a196cfc94e9feb9c93ab753933413
christo <christoslimit@gmail.com>
2022-07-01 11:32:01 +0530

    Frame: BackgroundColor and Frames

--- HelloSwiftUI/HelloSwiftUI/FrameBootCamp.swift
+++ HelloSwiftUI/HelloSwiftUI/FrameBootCamp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 //Adaptive Frame
 struct FrameBootCamp: View {
     var body: some View {
+        VStack{
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .background(Color.green)
             .frame(height: 100)
@@ -22,7 +23,9 @@ struct FrameBootCamp: View {
             .frame(height:400)
             .background(Color.green)
             .frame(maxHeight:.infinity, alignment: .top)
-            .background(Color.black)
+            .background(Color.white)
+            
+            FramePractice()
         
             //.frame(width: 300, height: 300, alignment: .leading)
             //.frame(minWidth: 0, idealWidth: 100, maxWidth: .infinity, minHeight: <#T##CGFloat?#>, idealHeight: <#T##CGFloat?#>, maxHeight: <#T##CGFloat?#>, alignment: <#T##Alignment#>)
@@ -30,10 +33,26 @@ struct FrameBootCamp: View {
             //.padding()
             //.background(Color.red)
     }
+    }
 }
 
 struct FrameBootCamp_Previews: PreviewProvider {
     static var previews: some View {
         FrameBootCamp()
+    }
+}
+
+struct FramePractice: View {
+    var body : some View {
+        Text("Hello Practice")
+            .background(Color.red)
+            .frame(height: 100)
+            .background(Color.green)
+            .frame(width: 150)
+            .background(Color.red)
+            .frame(maxWidth: .infinity)
+            .background(Color.indigo)
+            .frame(height: 300)
+            .background(.blue)
     }
 }
