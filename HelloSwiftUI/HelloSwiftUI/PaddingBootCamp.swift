40e68634ad8c11611ee5c80b561c9b2bf8ee2039
christo <christoslimit@gmail.com>
2022-07-01 14:01:04 +0530

    Padding practice

--- HelloSwiftUI/HelloSwiftUI/PaddingBootCamp.swift
+++ HelloSwiftUI/HelloSwiftUI/PaddingBootCamp.swift
@@ -10,13 +10,18 @@ import SwiftUI
 struct PaddingBootCamp: View {
     var body: some View {
         VStack(alignment: .leading) {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .padding(.bottom, 10)
-            Text("Here I am this is me there no where else on earth I rather be . Here I am just me and you tonight we will make our dreams come true.")
+            VStack (alignment: .leading){
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 10)
+                HStack {
+                    Text("ghghhhghghghghghhgh")
+                    Spacer()
+                }
+            }
+            
         }.padding()
-            .padding(.vertical, 10)
             .background(
                 Color.white
                     .cornerRadius(10)
@@ -31,5 +36,36 @@ struct PaddingBootCamp: View {
 struct PaddingBootCamp_Previews: PreviewProvider {
     static var previews: some View {
         PaddingBootCamp()
+.previewInterfaceOrientation(.portrait)
+    }
+}
+
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
+
+struct PaddingPractice: View {
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            Text("Heading")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding(.bottom, 10)
+            Text("")
+    
+        }
+        .padding()
+            .padding(.vertical, 10)
+            .background(
+                Color.white
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.3),
+                            radius: 10,
+                            x: 0,
+                            y: 10)
+            )
     }
 }
