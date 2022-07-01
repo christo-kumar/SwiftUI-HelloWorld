1d78a08c541938057f6b362a84860c398ae6f064
christo <christoslimit@gmail.com>
2022-07-01 10:36:46 +0530

    Custom Color

--- HelloSwiftUI/HelloSwiftUI/ColorBootCamp.swift
+++ HelloSwiftUI/HelloSwiftUI/ColorBootCamp.swift
@@ -9,12 +9,15 @@ import SwiftUI
 
 struct ColorBootCamp: View {
     var body: some View {
-        RoundedRectangle(cornerRadius: 25)
+        /*RoundedRectangle(cornerRadius: 25)
             .fill(
                 Color(#colorLiteral(red: 0.966827333, green: 0.6351944804, blue: 0.7092018723, alpha: 1))
             )
             .frame(width: 300, height: 300)
-            .shadow(color: Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)), radius: 1, x: 0, y: 10)
+            .shadow(color: Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)), radius: 1, x: 0, y: 10)*/
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color("Custo"))
+            .frame(width: 300, height: 300)
             
     }
 }
@@ -22,5 +25,6 @@ struct ColorBootCamp: View {
 struct ColorBootCamp_Previews: PreviewProvider {
     static var previews: some View {
         ColorBootCamp()
+            .preferredColorScheme(.dark)
     }
 }
