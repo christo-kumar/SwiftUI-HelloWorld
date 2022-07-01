7c12fe3a4885b1555968b5959e47a8ca70f26038
christo <christoslimit@gmail.com>
2022-07-01 11:04:48 +0530

    Image - resizeable, aspect ratio, frame, foreground color, backgroud color

--- HelloSwiftUI/HelloSwiftUI/IconBootCamp.swift
+++ HelloSwiftUI/HelloSwiftUI/IconBootCamp.swift
@@ -9,19 +9,34 @@ import SwiftUI
 
 struct IconBootCamp: View {
     var body: some View {
+        VStack {
         Image(systemName: "heart.fill")
             //.foregroundColor(Color(#colorLiteral(red:0.9, green: 0.4, blue: 0.7, alpha:1.0)))
             .foregroundColor(.green)
         //.resizable
         //.aspectRatio(contentMode: .fill)
             .font(.system(size: 50))
-            
             .frame(width: 200, height: 200, alignment: .center)
+            IconPractice()
+    }
     }
 }
 
 struct IconBootCamp_Previews: PreviewProvider {
     static var previews: some View {
         IconBootCamp()
+    }
+}
+
+struct IconPractice: View {
+    var body: some View {
+        Image(systemName: "person.fill")
+            //.font(.system(size: 50))
+            .resizable() //vs Font
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 200, height: 200, alignment: .center)
+            .foregroundColor(Color(#colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)))
+            .background(Color.gray)
+            
     }
 }
