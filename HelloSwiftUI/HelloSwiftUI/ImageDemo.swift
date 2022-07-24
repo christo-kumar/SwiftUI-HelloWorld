9c0965d61ad5a47a14203e02a1c4aab1d93a6600
christo <christoslimit@gmail.com>
2022-07-24 15:19:18 +0530

    Animation added.

--- HelloSwiftUI/HelloSwiftUI/ImageDemo.swift
+++ HelloSwiftUI/HelloSwiftUI/ImageDemo.swift
@@ -8,47 +8,74 @@
 import SwiftUI
 
 struct ImageDemo: View {
+    @State var changePosition = false
     var body: some View {
         VStack{
             Text("Friend Circle")
+            Button("Animate") {
+                            withAnimation(.easeInOut(duration: 2)) {
+                                changePosition.toggle()
+                            }
+                        }
             HStack{
                 Circle()
                     .fill(.blue)
                     .frame(width: 150, height: 150)
+                    .offset(x: changePosition ? 1000 : 0,
+                            y: changePosition ? 3000 : 0)
                     .overlay {
                         Image("Nannu")
                             .resizable().frame(width: 100, height: 100)
                             .aspectRatio(
                                 contentMode: .fit)
                             .clipped()
+                            .clipShape(Circle())
+                            .offset(x: changePosition ? 1000 : 0,
+                                    y: changePosition ? 3000 : 0)
                     }
                 Circle()
                     .fill(.red)
                     .frame(width: 150, height: 150)
+                    .offset(x: changePosition ? -1000 : 0,
+                            y: changePosition ? 3000: 0)
                     .overlay {
                         Image("Pihu")
                             .resizable()
                             .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .offset(x: changePosition ? -1000 : 0,
+                                    y: changePosition ? 3000 : 0)
                     }
             }
             
             HStack {
-                Rectangle()
+                Circle()
                     .fill(.green)
                     .frame(width: 150, height: 150)
+                    .offset(x: changePosition ?  1000 : 0,
+                            y: changePosition ? -500 : 0)
                     .overlay {
                         Image("Gunni")
                             .resizable()
                             .aspectRatio(
                             contentMode: .fit)
                             .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .offset(x: changePosition ?  1000 : 0,
+                                    y: changePosition ? -500: 0)
+                        
                     }
                 //Prof_Pic2
-                Rectangle().fill(.purple)
+                Circle().fill(.purple)
                     .frame(width: 150, height: 150)
+                    .offset(x: changePosition ?  -5000 : 0,
+                            y: changePosition ? -5000 : 0)
                     .overlay {
                         Image("Prof_Pic2").resizable()
                             .frame(width: 100, height: 100)
+                            .clipShape(Circle())
+                            .offset(x: changePosition ?  -5000 : 0,
+                                    y: changePosition ? -5000 : 0)
                     }
                 
             }
